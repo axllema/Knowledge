f23c76bd576b970ba779cd8fcf6aae30f8dfe3ed
axllema <axellexm@gmail.com>
2024-05-02 15:09:42 +0200

    loader and all

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,20 +1,12 @@
 import PropTypes from 'prop-types';
+import Logo from '../components/Logo';
 import '../scss/style.scss';
 import '../scss/components/_loader.scss';
 
-
 const Loader = ({ visible }) => {
-    const loadingText = ["K", "A", "S", "A"];
-
     return (
         <div className={`loadingScreen ${visible ? 'visible' : ''}`}>
-            <h3 className="loadingScreen__text">
-                {loadingText.map((val, index) => (
-                    <span key={index} className="loadingScreen__text__span">
-                        {val}
-                    </span>
-                ))}
-            </h3>
+            <Logo className="loadingScreen__logo" imageClassName="loadingScreen__logo-image"/>
         </div>
     );
 };
