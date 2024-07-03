3653e068c16e84e559c980ccac1702fdff59cdcb
axllema <axellexm@gmail.com>
2024-07-03 22:36:43 +0200

    changes in header, login page scss, create account + create account scss

--- src/pages/CreateAccount.jsx
+++ src/pages/CreateAccount.jsx
@@ -6,16 +6,40 @@ import '../scss/style.scss';
 
 
 function CreateAccount() {
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // Votre logique de gestion de la soumission du formulaire ici
+    };
+
     return (
-        <div>
-            <Header/>
+        <div className="create-account">
+            <Header />
+            <div className="create-account-container">
+                <main className="create-account-container-content">
+                    <h1>Créer un compte</h1>
 
-            <div>
-                page pour créer un compte !
-                - work in progress
+                    <form onSubmit={handleSubmit}>
+                        <div className="field-content">
+                            <label htmlFor="username">Nom d&rsquo;utilisateur</label>
+                            <input type="text" id="username" name="username" placeholder="Nom d'utilisateur" />
+                        </div>
+
+                        <div className="field-content">
+                            <label htmlFor="email">Email</label>
+                            <input type="email" id="email" name="email" placeholder="Email" />
+                        </div>
+
+                        <div className="field-content">
+                            <label htmlFor="password">Mot de passe</label>
+                            <input type="password" id="password" name="password" placeholder="Mot de passe" />
+                        </div>
+
+                        <button type="submit" className="create-account-button">Créer un compte</button>
+                        <a href="/login" className="sign-in-link">Déjà un compte ? Connectez-vous</a>
+                    </form>
+                </main>
             </div>
-
-            <Footer/>
+            <Footer />
         </div>
     );
 }
